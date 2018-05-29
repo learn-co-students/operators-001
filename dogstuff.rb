417b5c2f3ee2150c260f4b3ea6c2a3f6fad51541
Tony Turetsky <TTuretsky@gmail.com>
2018-05-28 20:50:49 -0400

    Done.

---
+++ dogstuff.rb
@@ -0,0 +1,14 @@
+smelly_smells gets.chomp
+
+case smelly_smells
+
+  when betty
+    puts "UGGGHHH BETTY YOU STINK WTF I HATE YOUR BUTTSMELLS"
+  when olive
+    puts "Babe, we need to clean Olive's butthole"
+  when lamby
+    puts "LAMBY GET YOUR BUTT OUT OF MY FACE!"
+  when bagheera
+    puts "Aww Bagheera you little stinker i love you memers"
+  end
+end
